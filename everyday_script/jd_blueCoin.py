06a20fe9c90a7e00cbf24df94adb085fa747c828
Superfice <wjyi@vip.qq.com>
2021-08-30 08:57:38 +0800

    Add files via upload

--- everyday_script/jd_blueCoin.py
+++ everyday_script/jd_blueCoin.py
@@ -1,26 +1,19 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*
 '''
-项目名称: JD-Script / jd_blueCoin
-Author: Curtin
-功能: 东东超市商品兑换
-Date: 2021/4/17 上午11:22
-update: 2021/7/17 01:15
-TG交流 https://t.me/topstyle996
-TG频道 https://t.me/TopStyle2021
-建议cron: 59 23 * * *  python3 jd_blueCoin.py
+cron: 59 23 * * * jd_blueCoin.py
 '''
 ################【参数】######################
 # ck 优先读取【JDCookies.txt】 文件内的ck  再到 ENV的 变量 JD_COOKIE='ck1&ck2' 最后才到脚本内 cookies=ck
 #ENV设置：export JD_COOKIE='cookie1&cookie2'
 cookies = ''
 #【填写您要兑换的商品】ENV设置： export coinToBeans='京豆包'
-coinToBeans = '京豆包'
+coinToBeans = ''
 
 #多账号并发，默认关闭 ENV设置开启： export blueCoin_Cc=True
-blueCoin_Cc = True
+blueCoin_Cc = False
 #单击次数
-dd_thread = 8
+dd_thread = 3
 ###############################################
 
 import time, datetime, os, sys, random
